1ef846c875fe55d5c2a23926aea01f3a800c4869
rohit0987 <rohithamlpur@gmail.com>
2024-02-23 13:57:42 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Overview from './pages/Overview';
+import Amenities from './pages/Amenities';
+import Gallery from './pages/Gallery';
+import FloorPlan from './pages/FloorPlan';
+import Location from './pages/Location';
+import Contact from './pages/Contact';
+
+
 
 function App() {
   return (
-    <h1 className='text-red-500'>App</h1>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Overview />}/>
+    <Route path="/amenities" element={<Amenities />}/>
+    <Route path="/gallery" element={<Gallery />}/>
+    <Route path="/floor-plan" element={<FloorPlan />}/>
+    <Route path="/location" element={<Location/>}/>
+    <Route path="/contact" element={<Contact/>}/>
+    </Routes>
+ 
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/FloorPlan.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FloorPlan() {
+  return (
+    <div>FloorPlan</div>
+  )
+}
+
+export default FloorPlan
